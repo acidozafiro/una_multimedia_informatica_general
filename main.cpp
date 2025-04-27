b096c462e77df87f54bbd158b8841bfbb4089a75
Macarena Susel <macarena_vaulet@outlook.com>
2025-04-27 19:03:50 -0300

    Update main.cpp
    tipo de triangulo

--- main.cpp
+++ main.cpp
@@ -1,10 +1,38 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-  int x = 0
-  while {x < 10
-  x ++ }
-  return 0;
+#include <iostream>
+using namespace std;
+
+int main() {
+    float lado1, lado2, lado3;
+
+    // Ingreso de datos
+    cout << "Ingrese la longitud del primer lado: ";
+    cin >> lado1;
+    cout << "Ingrese la longitud del segundo lado: ";
+    cin >> lado2;
+    cout << "Ingrese la longitud del tercer lado: ";
+    cin >> lado3;
+
+    // Verificar si forma un triángulo válido
+    if (lado1 > 0 && lado2 > 0 && lado3 > 0 &&
+        lado1 + lado2 > lado3 &&
+        lado1 + lado3 > lado2 &&
+        lado2 + lado3 > lado1) {
+        
+        // Determinar el tipo de triángulo
+        if (lado1 == lado2 && lado2 == lado3) {
+            cout << "El triangulo es EQUILATERO." << endl;
+        } else if (lado1 == lado2 || lado1 == lado3 || lado2 == lado3) {
+            cout << "El triangulo es ISOSCELES." << endl;
+        } else {
+            cout << "El triangulo es ESCALENO." << endl;
+        }
+
+    } else {
+        cout << "Las medidas ingresadas NO forman un triangulo valido." << endl;
+    }
+
+    return 0;
 }
